590bcf3d19725905997cb6aec2b2ab88899fa228
Kundan Singh <75195602+ku281@users.noreply.github.com>
2022-01-03 21:29:53 +0530

    Update Post.jsx

--- src/Component/home/Post.jsx
+++ src/Component/home/Post.jsx
@@ -23,7 +23,7 @@ const Post =()=>{
     const classes=useStyles();
    // const url =" https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80create.jsx"
    //const url= "https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80create.jsx";
-   const url="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80create.jsx";
+   const url="https://www.gstatic.com/webp/gallery/3.jpg";
    //const url="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80create.jsx ";
     return(
         <Box className ={classes.container}>
